14c1c55130a85a750b79edb5e56f444ae7256a6a
Jay Bazuzi <jay@bazuzi.com>
2026-09-03 04:21:40 +0000

    fix test

--- Tests/StrongGuidTests.cs
+++ Tests/StrongGuidTests.cs
@@ -6,6 +6,6 @@ public class StrongGuidTests
     [Fact]
     public void CanBeConstructed()
     {
-        var _ = new StrongGuid(new UserId(Guid.NewGuid()), new UserId(Guid.NewGuid()));
+        var _ = new StrongGuid(new UserId(Guid.NewGuid()), new UserId(Guid.NewGuid()), new UserId(Guid.NewGuid()));
     }
 }

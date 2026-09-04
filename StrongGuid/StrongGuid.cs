0aaf5f3b224aa9597341fa630991937da4d274fd
Jay Bazuzi <jay@bazuzi.com>
2026-09-04 05:15:01 +0000

    User/DirectoryObject example

--- StrongGuid/StrongGuid.cs
+++ StrongGuid/StrongGuid.cs
@@ -1,24 +1,16 @@
-public readonly record struct UserId(Guid Value);
+
 
 public class StrongGuid
 {
-    public UserId momUserId;
-    public UserId dadUserId;
-    private UserId _childUserId;
+    public string? _entraObjectId;
 
-
-    public StrongGuid(UserId momUserId, UserId dadUserId, UserId childUserId)
+    public void G(Microsoft.Graph.Models.User entraUser)
     {
-        this.momUserId = momUserId;
-        this.dadUserId = dadUserId;
-
-        this._childUserId = childUserId;
-
-    }
-
-    void F()
-    {
-        momUserId = dadUserId; // warning SIA001: Value with [Id("DadUser")] is assigned to a target with [Id("MomUser")]
+        if (entraUser.Id == null)
+        {
+            throw new ArgumentNullException(nameof(entraUser));
+        }
+        _entraObjectId = entraUser.Id; // warning SIA001: Value with [Id("User/DirectoryObject")] is assigned to a target with [Id("EntraObject")]
     }
 }
 
--- Tests/StrongGuidTests.cs
+++ Tests/StrongGuidTests.cs
@@ -6,6 +6,7 @@ public class StrongGuidTests
     [Fact]
     public void CanBeConstructed()
     {
-        var _ = new StrongGuid(new UserId(Guid.NewGuid()), new UserId(Guid.NewGuid()), new UserId(Guid.NewGuid()));
+        var _ = new StrongGuid();
+
     }
 }
